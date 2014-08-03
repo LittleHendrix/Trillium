11dbb6755f2a2f16543ae55b520bf40978ebc304
Chen Lu <chenlu.design@gmail.com>
2014-08-04 00:03:09 +0100

    fixed blog sortBy multiple datetime fields issue - not sort by 'publishDate' first then by @createDate

---
+++ Trillium/Controllers/BlogController.cs
@@ -0,0 +1,46 @@
+﻿namespace Trillium.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using Trillium.Models;
+
+    using Umbraco.Core.Models;
+    using Umbraco.Web;
+    using Umbraco.Web.Mvc;
+
+    public class BlogController : RenderMvcController
+    {
+        [OutputCache(Duration = 60, VaryByParam = "*")]
+        public ActionResult Blog(Blog model)
+        {
+            model.BlogPost = GetPagedBlogPost(model);
+            //model.LastUpdated = DateTime.Now;
+            return this.CurrentTemplate(model);
+        }
+
+        private static IEnumerable<IPublishedContent> GetPagedBlogPost(Blog model)
+        {
+            if (model.Page == default(int))
+            {
+                model.Page = 1;
+            }
+
+            var pageSise = model.Content.HasValue("postsPerPage") ? Convert.ToInt32(model.Content.GetPropertyValue("postsPerPage")) : 5;
+            var skipItems = (pageSise * model.Page) - pageSise;
+
+            var posts = model.Content.Children.Where(x => x.IsVisible()).OrderByDescending(x => x.HasValue("publishDate") ? x.GetPropertyValue<DateTime>("publishDate") : x.CreateDate).ToList();
+            model.TotalPages = Convert.ToInt32(Math.Ceiling((double)posts.Count() / pageSise));
+
+            model.PreviousPage = model.Page - 1;
+            model.NextPage = model.Page + 1;
+
+            model.IsFirstPage = model.Page <= 1;
+            model.IsLastPage = model.Page >= model.TotalPages;
+
+            return posts.Skip(skipItems).Take(pageSise);
+        }
+    }
+}
---
+++ Trillium/Models/Blog.cs
@@ -0,0 +1,29 @@
+﻿namespace Trillium.Models
+{
+    using System.Collections.Generic;
+    using Umbraco.Core.Models;
+    using Umbraco.Web;
+    using Umbraco.Web.Models;
+
+    public class Blog : RenderModel
+    {
+        public Blog()
+            : base(UmbracoContext.Current.PublishedContentRequest.PublishedContent)
+        {
+        }
+
+        public int Page { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public int PreviousPage { get; set; }
+
+        public int NextPage { get; set; }
+
+        public bool IsFirstPage { get; set; }
+
+        public bool IsLastPage { get; set; }
+
+        public IEnumerable<IPublishedContent> BlogPost { get; set; }
+    }
+}
