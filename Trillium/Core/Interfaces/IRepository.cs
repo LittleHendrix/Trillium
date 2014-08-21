ff219eb28583d2ff47a1ac295b6a4a08ea11b86c
Chen Lu <chenlu.design@gmail.com>
2014-08-21 22:56:42 +0100

    added generic iRepository interface

---
+++ Trillium/Core/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Trillium.Core.Interfaces
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    interface IRepository<T> where T : class
+    {
+        IQueryable<T> SearchFor(Expression<Func<T, bool>> predicate);
+        IQueryable<T> GetAll();
+        T GetById(int id);
+    }
+
+    interface IEditableRepository<T> : IRepository<T> where T : class
+    {
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
