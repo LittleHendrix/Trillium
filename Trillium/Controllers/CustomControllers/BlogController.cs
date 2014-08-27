5fb84716acccb1346e8a176b25d9fdec5c9a0541
Chen Lu <chenlu.design@gmail.com>
2014-08-27 23:58:04 +0100

    installed autofac, added IoC to ApplicationEventHandler

--- Trillium/Controllers/CustomControllers/BlogController.cs
+++ Trillium/Controllers/CustomControllers/BlogController.cs
@@ -12,28 +12,37 @@
 
     public class BlogController : RenderMvcController
     {
+        private readonly BlogViewModel _blogViewModel;
+
+        public BlogController(BlogViewModel blogViewModel)
+        {
+            _blogViewModel = blogViewModel;
+        }
+
         [OutputCache(Duration = 60, VaryByParam = "*")]
         public ActionResult Blog(RenderModel umbModel)
         {
-            var page = this.HttpContext.Request.QueryString["Page"] ?? "1";
-            var pageInt = Convert.ToInt32(page);
-            var vm = new BlogViewModel
-            {
-                Page = pageInt
-            };
+            string page = HttpContext.Request.QueryString["Page"] ?? "1";
+            int pageInt = Convert.ToInt32(page);
+            _blogViewModel.Page = pageInt;
+            _blogViewModel.BlogPost = GetPagedBlogPost(_blogViewModel);
 
-            vm.BlogPost = GetPagedBlogPost(vm);
-
-            return this.CurrentTemplate(vm);
+            return CurrentTemplate(_blogViewModel);
         }
 
         private static IEnumerable<IPublishedContent> GetPagedBlogPost(BlogViewModel model)
         {
-            var pageSise = model.Content.HasValue("itemsPerPage") ? Convert.ToInt32(model.Content.GetPropertyValue("itemsPerPage")) : model.PageSize;
-            var skipItems = (pageSise * model.Page) - pageSise;
+            int pageSise = model.Content.HasValue("itemsPerPage")
+                ? Convert.ToInt32(model.Content.GetPropertyValue("itemsPerPage"))
+                : model.PageSize;
+            int skipItems = (pageSise*model.Page) - pageSise;
 
-            var posts = model.Content.Children.Where(x => x.IsVisible()).OrderByDescending(x => x.HasValue("publishDate") ? x.GetPropertyValue<DateTime>("publishDate") : x.CreateDate).ToList();
-            model.TotalPages = Convert.ToInt32(Math.Ceiling((double)posts.Count() / pageSise));
+            List<IPublishedContent> posts =
+                model.Content.Children.Where(x => x.IsVisible())
+                    .OrderByDescending(
+                        x => x.HasValue("publishDate") ? x.GetPropertyValue<DateTime>("publishDate") : x.CreateDate)
+                    .ToList();
+            model.TotalPages = Convert.ToInt32(Math.Ceiling((double) posts.Count()/pageSise));
 
             model.PreviousPage = model.Page - 1;
             model.NextPage = model.Page + 1;
---
+++ Trillium/Extensions/EventHandlers/TrilliumApplication.cs
@@ -0,0 +1,32 @@
+﻿namespace Trillium.Extensions.EventHandlers
+{
+    using System.Web.Mvc;
+    using Autofac;
+    using Autofac.Integration.Mvc;
+    using Trillium.ViewModels;
+    using Umbraco.Core;
+
+    public class TrilliumApplication : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication,
+            ApplicationContext applicationContext)
+        {
+            SetupDependencyInjection();
+        }
+
+        private void SetupDependencyInjection()
+        {
+            var builder = new ContainerBuilder();
+
+            // register all controllers found in this assembly
+            builder.RegisterControllers(typeof (TrilliumApplication).Assembly);
+
+            // add custom class to the container as Transient instance
+            builder.RegisterType<BlogViewModel>();
+
+            IContainer container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
