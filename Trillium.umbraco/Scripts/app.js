44b081f21163ba02de8800aa3f5a7a2e9f63f9dc
Chen Lu <chenlu.design@gmail.com>
2015-03-10 21:15:38 +0000

    installed skrollr, added parallax header

--- Trillium.umbraco/Scripts/app.js
+++ Trillium.umbraco/Scripts/app.js
@@ -2,39 +2,41 @@
 
     $(document).foundation();
 
-    initTypeAhead();
+    skrollr.init();
 
-    function initTypeAhead() {
-        var results = {};
-        $('.typeahead').typeahead({
-            minLength: 3,
-            items: 20,
-            source: function (query, process) {
-                $.post('/search?json=true&searchTerms=' + query, { q: query, limit: 8 }, function (data) {
-                    titles = [];
-                    results = {};
-                    $.each(data, function (i, result) {
-                        results[result.Title] = result;
-                        titles.push(result.Title);
-                    });
-                    process(titles);
-                });
-            },
-            matcher: function (item) { return true; },
-            updater: function (item) {
-                var selectedValue = '';
-                if (item == undefined) {
-                    selectedValue = $('.typeahead').val();
-                    $('.typeahead').closest('form').submit();
-                }
-                else {
-                    selectedValue = item;
-                    window.location = results[item].Url;
-                }
-                return $('.typeahead').val();
-            }
-        });
-    }
+    //initTypeAhead();
+
+    //function initTypeAhead() {
+    //    var results = {};
+    //    $('.typeahead').typeahead({
+    //        minLength: 3,
+    //        items: 20,
+    //        source: function (query, process) {
+    //            $.post('/search?json=true&searchTerms=' + query, { q: query, limit: 8 }, function (data) {
+    //                titles = [];
+    //                results = {};
+    //                $.each(data, function (i, result) {
+    //                    results[result.Title] = result;
+    //                    titles.push(result.Title);
+    //                });
+    //                process(titles);
+    //            });
+    //        },
+    //        matcher: function (item) { return true; },
+    //        updater: function (item) {
+    //            var selectedValue = '';
+    //            if (item == undefined) {
+    //                selectedValue = $('.typeahead').val();
+    //                $('.typeahead').closest('form').submit();
+    //            }
+    //            else {
+    //                selectedValue = item;
+    //                window.location = results[item].Url;
+    //            }
+    //            return $('.typeahead').val();
+    //        }
+    //    });
+    //}
 
     $('a[href*=#]:not([href=#])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
